598b01c94ab19464cb899ad89ee2837b15e6767b
lukeknightdev <lukeknightdev@gmail.com>
2021-12-28 11:22:16 +0000

    added bug fix 0.001

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const timeContainer = document.getElementById("time-container");
 const birthDay = "05-03-2002";
 const birthDayDate = new Date(birthDay);
-const int1NumberFormatter = new Int1.NumberFormat("en-US");
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
 
 setInterval(() => {
     const now = new Date();
     const difference = Math.floor((now.getTime() - birthDayDate.getTime()) / 1000);
 
-    timeContainer.innerText = int1NumberFormatter.format(difference);
+    timeContainer.innerText = intlNumberFormatter.format(difference);
 }, 1000);
