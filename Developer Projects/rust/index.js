6cd0f91e4796f5b11f587735165887f450c92184
lukeknightdev <lukeknightdev@gmail.com>
2022-01-02 23:47:33 +0000

    Solana Airdrop Program on Devnet

---
+++ Developer Projects/rust/index.js
@@ -0,0 +1,59 @@
+//Generating Dev Wallet
+
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    Transaction,
+    Account,
+   } = require("@solana/web3.js");
+
+// Public Key & Private Key
+
+   const newPair = new Keypair();
+
+   const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
+
+   const secretKey = newPair._keypair.secretKey
+
+// Wallet Balance Function
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = await Keypair.fromSecretKey(secretKey);
+        const walletBalance = await connection.getBalance(
+            new PublicKey(myWallet.publicKey)
+        );
+        console.log(`=> For wallet address ${publicKey}`);
+        console.log(`   Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//Airdropping Sol Function
+
+const airDropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(walletKeyPair.publicKey),
+            2 * LAMPORTS_PER_SOL
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//Creating Driver Function To Test other Functions
+
+const driverFunction = async () => {
+    await getWalletBalance();
+    await airDropSol();
+    await getWalletBalance();
+}
