ea546dcc4457ecbaeea32effcadea679a2fafeba
lukeknightdev <lukeknightdev@gmail.com>
2022-01-08 14:05:33 +0000

    Wallet setup for devnet & airdrop sol function made

---
+++ Developer Projects/rust/index.js
@@ -0,0 +1,63 @@
+//Adding required modules for Solana
+
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    Transaction,
+    Account,
+   } = require("@solana/web3.js");
+
+//Kepair module above^, allows us to create a new wallet
+
+const newPair = new Keypair();
+
+//Storing the wallet details - extracting (accountInfo) and storing in new variable
+
+const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
+
+const secretKey = newPair._keypair.secretKey
+
+//Creating the Wallet Balance Function
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = await Keypair.fromSecretKey(secretKey);
+        const walletBalance = await connection.getBalance(
+            new PublicKey(myWallet.publicKey)
+        );
+        console.log(`=> For wallet address ${publicKey}`);
+        console.log(`   Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//Airdrop 2 Sol Function
+
+const airDropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+        console.log(`-- Airdropping 2 SOL --`)
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(walletKeyPair.publicKey),
+            2 * LAMPORTS_PER_SOL
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//driver funciton test
+
+const driverFunction = async () => {
+    await getWalletBalance();
+    await airDropSol();
+    await getWalletBalance();
+}
+driverFunction();
