fd8142f806794a478368fb496b7066b6f9d08e00
lukeknightdev <lukeknightdev@gmail.com>
2022-01-08 12:48:47 +0000

    Restarting the project

--- Developer Projects/rust/project 1 - airdrop/hello-world/target/debug/deps/hello_world-6086fa234683b63a.d
+++
@@ -1,5 +0,0 @@
-/home/lukeknightdev/Developer Projects/rust/hello-world/target/debug/deps/hello_world-6086fa234683b63a.rmeta: src/main.rs
-
-/home/lukeknightdev/Developer Projects/rust/hello-world/target/debug/deps/hello_world-6086fa234683b63a.d: src/main.rs
-
-src/main.rs:
--- Developer Projects/rust/project 1 - airdrop/hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/home/lukeknightdev/Developer\ Projects/rust/hello-world/target/debug/hello-world: /home/lukeknightdev/Developer\ Projects/rust/hello-world/src/main.rs
